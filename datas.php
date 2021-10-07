f6b617bca34e4db0adc8e781fdb9d2da22031bf5
darkstrr <34400746+darkstrr@users.noreply.github.com>
2021-10-07 15:32:30 -0400

    Added basic list for temp database

---
+++ datas.php
@@ -0,0 +1,10 @@
+<?php
+$dataUser = [
+	["id" => 'admin' , "password" => 'password', "perm" => 4],
+	["id" => 'user' , "password" => 'user', "perm" => 1],
+];
+
+$dataBooks = [
+	["id" => 1, "title" => 'Random book Title', "isbn10" => 1234567891, "isbn13" => 1234567891234, "author" => 'author'],
+];
+?>
